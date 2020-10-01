aec27a77b35c16b9bbeac37196420cc9ecc793f0
Yifei <valkriaine@outlook.com>
2020-09-30 19:54:56 -0700

    setup basic layout and design logic

---
+++ app/src/main/java/com/smartdigital/medicine/UserMedicine.java
@@ -0,0 +1,18 @@
+package com.smartdigital.medicine;
+
+public class UserMedicine
+{
+    private String name;
+    //todo: add more medicine information here
+
+    public UserMedicine(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+}
