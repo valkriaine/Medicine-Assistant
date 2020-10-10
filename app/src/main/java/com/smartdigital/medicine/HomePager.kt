e3dd6a4d330e55a4a8bfb39ddff41ade5041c3d4
Yifei <valkriaine@outlook.com>
2020-10-10 01:54:49 -0700

    code cleanup

--- app/src/main/java/com/smartdigital/medicine/HomePager.kt
+++ app/src/main/java/com/smartdigital/medicine/HomePager.kt
@@ -1,6 +1,5 @@
 package com.smartdigital.medicine
 
-import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
